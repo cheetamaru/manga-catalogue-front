d182675713bd15750cd45bc8cb7e66acb8a20526
Cheetamaru <vitalvat@gmail.com>
2022-12-01 22:56:42 +0200

    feat: add automatic type of httpPlugin in nuxtApp

--- plugins/httpClientPlugin.ts
+++ plugins/httpClientPlugin.ts
@@ -1,11 +1,15 @@
 import { HttpClient } from "@/services/api/HttpClient"
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
     const baseURL = 'http://127.0.0.1:8000/api'
 
     const fetchClient = $fetch.create({ baseURL })
 
     const httpClient = new HttpClient(fetchClient)
 
-    nuxtApp.provide('httpClient', httpClient)
+    return {
+        provide: {
+            httpClient,
+        }
+    }
 })
