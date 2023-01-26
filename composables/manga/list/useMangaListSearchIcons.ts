9fec6a98c3742175d239c1bca934cdaad2d74f49
cheetamaru <vitalvat@gmail.com>
2023-01-26 22:55:36 +0200

    refactor: move icon names to constants

--- composables/manga/list/useMangaListSearchIcons.ts
+++ composables/manga/list/useMangaListSearchIcons.ts
@@ -1,12 +1,16 @@
 import { ComputedRef } from 'vue'
 
+const deleteIconName = 'mdi-delete-sweep'
+const filterIconClearedName = 'mdi-filter-menu-outline'
+const filterIconFilledName = 'mdi-filter-menu'
+
 export const useMangaListSearchIcons = (areSidebarFiltersEmpty: ComputedRef<boolean>) => {
   const appendInnerIcon = computed(() => {
-    return areSidebarFiltersEmpty.value ? undefined : 'mdi-delete-sweep'
+    return areSidebarFiltersEmpty.value ? undefined : deleteIconName
   })
 
   const appendIcon = computed(() => {
-    return areSidebarFiltersEmpty.value ? 'mdi-filter-menu-outline' : 'mdi-filter-menu'
+    return areSidebarFiltersEmpty.value ? filterIconClearedName : filterIconFilledName
   })
 
   return {
